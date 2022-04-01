9aa3f334f1d4ec259cac3916516611b4ca8af08f
lzc <821305833@qq.com>
2022-04-01 12:29:46 +0800

    Extract Variable

--- ComposingMethods/Extract.java
+++ ComposingMethods/Extract.java
@@ -1,19 +1,24 @@
 /**
- * The more lines found in a method, the harder it's to figure out what the method does.
  * @author Zechen Liu
  * @date 2022/03/28
  **/
 public class Extract {
+    //extracting variables is to make a complex expression more understandable
+    private static final boolean isWindows = System.getProperty("os.name").toUpperCase().contains("WINDOWS");
+
     public static void main(String[] args) {
-        String name = "Zechen Liu";
-        printDetails(name, 99.9);
+        if(isWindows) {
+            String name = "Zechen Liu";
+            printDetails(name, 99.9);
+        }
     }
 
     private static void printDetails(String name, double score) {
         System.out.println("name: " + name);
         System.out.println("score: " + score);
     }
-/*
+
+/*  The more lines found in a method, the harder it's to figure out what the method does.
     public static void main(String[] args) {
         String name = "Zechen Liu";
         double score = 99.9;
