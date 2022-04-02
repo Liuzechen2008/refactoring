07ff657037de827815dc99f93ae7710d0f0dfe9a
lzc <821305833@qq.com>
2022-04-02 09:32:39 +0800

    Split Temporary Variable

--- ComposingMethods/OneThingOnly.java
+++ ComposingMethods/OneThingOnly.java
@@ -1,9 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Zechen Liu
  * @date 2022/04/01
  **/
 public class OneThingOnly {
     public static void main(String[] args) {
-
+        List<Double> list = new ArrayList<>();
+        list.add(2.1);
+        list.add(2.0);
+        double[] len = getLength(list);
+        //double tmp = height * width;// Split Temporary Variable
+        final double area = len[0] * len[1];
+        System.out.println(area);
     }
+
+    private static double[] getLength(List<Double> list) {
+        double height = list.get(0);
+        double width = list.get(1);
+        return new double[]{height,width};
+    }
+
 }
